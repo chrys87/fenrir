903944ca250baf9227bffcb40637df2074101d4a
Storm Dragon <stormdragon2976@gmail.com>
2016-07-04 23:17:19 -0400

    Speech class added for es.py.

---
+++ src/speech/es.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Espeak driver
+
+class speech():
+    def __init__():
+        pass
+
+    def start_speech(text, queueable=True):
+        if queueable == False: stop_speech()
+
+    def stop_speech():
+        pass
+
+    def clear_speech_buffer():
+        pass
