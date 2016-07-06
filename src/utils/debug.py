03b81fcaef5461258d802807d85eb4b45b162e63
Storm Dragon <stormdragon2976@gmail.com>
2016-07-06 16:36:06 -0400

    this module is not finished, but I wanted to push it in case I need to work on it on the netbook. Half-complete debug.py.

---
+++ src/utils/debug.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Debugger module for the Fenrir screen reader.
+
+class debug():
+    def __init__(self, level=0i, debugFile=/var/log/fenrir.log)
+        self.debugLevel = level
+
+    def get_debug_level():
+        return debugLevel
+
+    def set_debug_level(level):
+        debugLevel = level
+
+def get_debug_file():
+        return debugFile
+
+    def set_debug_file(file):
+        debugFile = file
+
+
