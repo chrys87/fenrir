5f91077ba93278735ed0300fd1910a4541404bca
chrys87 <chrys87@web.de>
2016-07-08 11:22:40 +0200

    change structure

--- src/fenrir-package/core/environment.py
+++ src/fenrir-package/core/environment.py
@@ -8,19 +8,6 @@ from core import screenData
 from core import generalInformation
 
 environment = {
-'columns': 0,
-'lines': 0,
-'delta': '',
-'oldCursor':{'x':0,'y':0},
-'oldContentBytes': b'',
-'oldContentText': '',
-'oldContentAttrib': b'',
-'newCursor':{'x':0,'y':0},
-'newContentBytes': b'',
-'newContentText': '',
-'newContentAttrib': b'',
-'oldTTY':'-1', #to get shure that the first loop is a chagne
-'newTTY':'0',
 'screenData' = screenData.screenData,
 'runtime' = runtime.runtime,
 'generalInformation' = generalInformation.generalInformation,
