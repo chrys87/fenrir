d430583f511abe5134759bfc9bc7fe40641aab19
chrys87 <chrys87@web.de>
2016-07-08 11:23:31 +0200

    change structure

--- src/fenrir-package/core/screenData.py
+++ src/fenrir-package/core/screenData.py
@@ -1,4 +1,17 @@
 #!/bin/python
 
 screenData = {
+'columns': 0,
+'lines': 0,
+'delta': '',
+'oldCursor':{'x':0,'y':0},
+'oldContentBytes': b'',
+'oldContentText': '',
+'oldContentAttrib': b'',
+'newCursor':{'x':0,'y':0},
+'newContentBytes': b'',
+'newContentText': '',
+'newContentAttrib': b'',
+'oldTTY':'-1', #to get shure that the first loop is a chagne
+'newTTY':'0',
 }
