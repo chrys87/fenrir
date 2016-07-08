9b421ea5340aa8ba4cc93cd10af345816d3f3b13
chrys87 <chrys87@web.de>
2016-07-08 13:26:31 +0200

    change structure

---
+++ src/fenrir-package/commands/curr_line.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+class command():
+    def __init__(self):
+        pass
+    def run(self, environment):
+        pass
+    def setCallback(self, callback)
+        pass
+    def shutdown(self):
+        pass
