ea55d47fdc122e910785dbfcfe2afde30ff8fd55
chrys <chrys87@web.de>
2016-07-10 19:06:34 +0200

    keyboard stuff

--- src/fenrir-package/core/settings.py
+++ src/fenrir-package/core/settings.py
@@ -3,8 +3,18 @@
 from utils import debug
 
 settings = {
+'speechEnabled': True,
 'speechDriverString':'speechd',
+'speechRate': 1,
+'speechPitch': 1,
+'speechModule': '',
+'speechVoice': 'de',
 'screenDriverString': 'linux',
-'soundDriverString': 'sox',
-'debugLevel': debug.debugLevel.DEACTIVE
+'keyboardLayout': "desktop",
+'brailleEnabled': False, 
+'soundEnabled': False,
+'soundDriverString': 'sox', 
+'soundTheme': 'default',
+'debugLevel': debug.debugLevel.DEACTIVE,
+'punctuationLevel': 1 
 }
