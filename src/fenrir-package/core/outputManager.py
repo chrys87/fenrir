311e653893bca05698659dcba89e3431d7b9be5d
chrys <chrys87@web.de>
2016-07-13 11:15:19 +0200

    initial outputManager

---
+++ src/fenrir-package/core/outputManager.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+class outputManager():
+    def __init__(self):
+        pass
+    def presentText(self, environment, Text):
+        pass
+    def speakText(self, environment, Text):
+        pass
+    def brailleText(self, environment, Text):
+        pass
+    def interruptOutput(self, environment, Text):
+        pass
+    def playSoundIcon(self, environment, Text):
+        pass
