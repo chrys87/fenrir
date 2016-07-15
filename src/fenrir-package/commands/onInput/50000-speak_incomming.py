e2668f146770aad3247ede30ffee766d3bb37565
chrys <chrys87@web.de>
2016-07-15 15:56:47 +0200

    add horizontal and vertical announcements

--- src/fenrir-package/commands/onInput/50000-speak_incomming.py
+++ src/fenrir-package/commands/onInput/50000-speak_incomming.py
@@ -4,6 +4,10 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
+        return environment
+        if environment['screenData']['newCursor'] != environment['screenData']['oldCursor']:
+            print(environment['screenData']['newCursor'] != environment['screenData']['oldCursor'])
+            return environment
         if environment['screenData']['newDelta'] != environment['screenData']['oldDelta'] or \
           environment['screenData']['newTTY'] != environment['screenData']['oldTTY']:
             environment['runtime']['outputManager'].speakText(environment, environment['screenData']['newDelta'], Interrupt=False)
