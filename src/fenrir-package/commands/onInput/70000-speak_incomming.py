46fc7ed2189d77ec153cf53bbae1578f05d828f8
chrys <chrys87@web.de>
2016-07-23 17:08:00 +0200

    seperate onInput and onScreenUpdate incomming speech for prepare word echo and stuff

---
+++ src/fenrir-package/commands/onInput/70000-speak_incomming.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+class command():
+    def __init__(self):
+        pass
+    def run(self, environment):
+        if not environment['runtime']['settingsManager'].getSettingAsBool(environment, 'keyboard', 'charEcho') and\
+          environment['screenData']['newCursor']['x'] - environment['screenData']['oldCursor']['x'] == 1:
+            return environment 
+
+        ttyChanged = environment['screenData']['newTTY'] != environment['screenData']['oldTTY']
+        if environment['screenData']['newDelta'] == environment['screenData']['oldDelta'] and \
+          not ttyChanged:
+            return environment
+            
+        environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newDelta'], ttyChanged)
+        return environment
+    def setCallback(self, callback):
+        pass
+    def shutdown(self):
+        pass
