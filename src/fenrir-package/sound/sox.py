bd9e4dedc86e4878360b6cf44dbf9b9e41a9c3d4
chrys <chrys87@web.de>
2016-07-29 00:23:30 +0200

    initial sox

--- src/fenrir-package/sound/sox.py
+++ src/fenrir-package/sound/sox.py
@@ -1,12 +1,13 @@
 #!/bin/python
+import subprocess
 
 class sound():
     def __init__(self):
         pass
     def playFrequence(self, frequence, duration, adjustVolume):
         pass
-    def playSoundFile(self, filePath):
-        pass
+    def playSoundFile(self, filePath, interrupt = True):
+         subprocess.call("play " + filePath, shell=True)
     def cancel(self):
         pass        
     def setCallback(self, callback):
