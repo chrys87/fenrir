03722148f82e52fe21d141b9d4c36dba5d5fbfba
Chrys <chrys87@web.de>
2016-08-03 19:43:23 +0200

    initial rebuild of onInput commands

--- src/fenrir-package/commands/onInput/70000-speak_incomming.py
+++ src/fenrir-package/commands/onInput/70000-speak_incomming.py
@@ -4,17 +4,15 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
-        if not environment['runtime']['settingsManager'].getSettingAsBool(environment, 'keyboard', 'charEcho') and\
-          environment['screenData']['newCursor']['x'] - environment['screenData']['oldCursor']['x'] == 1:
+        if environment['screenData']['newCursor']['x'] > environment['screenData']['oldCursor']['x']:
             return environment 
 
-        ttyChanged = environment['screenData']['newTTY'] != environment['screenData']['oldTTY']
-        if environment['screenData']['newDelta'] == environment['screenData']['oldDelta'] and \
-          not ttyChanged:
+        if environment['screenData']['newTTY'] != environment['screenData']['oldTTY']:
             return environment
-        if ttyChanged:
-            environment['runtime']['outputManager'].playSoundIcon(environment,'ChangeTTY')            
-        environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newDelta'], interrupt=ttyChanged)
+        if environment['screenData']['newDelta'] == environment['screenData']['oldDelta']:
+            return environment
+           
+        environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newDelta'], interrupt=True)
 
         return environment
     def setCallback(self, callback):
