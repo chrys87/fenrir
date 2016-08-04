f88507f6ffa732c2b91b06eef6e7068cf9be5335
chrys <chrys87@web.de>
2016-08-05 00:47:41 +0200

    fix toggle speech

--- src/fenrir-package/commands/commands/toggle_speech_enable.py
+++ src/fenrir-package/commands/commands/toggle_speech_enable.py
@@ -5,8 +5,8 @@ class command():
         pass
     def run(self, environment):
         
-        if environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'enabled',soundIconName='SpeechOff', interrupt=True): 
-            environment['runtime']['outputManager'].presentText(environment, "speech disabled")
+        if environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'enabled'): 
+            environment['runtime']['outputManager'].presentText(environment, "speech disabled",soundIconName='SpeechOff', interrupt=True)
         environment = environment['runtime']['settingsManager'].setSetting(environment, 'speech', 'enabled', str(not environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'enabled')))   
         if environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'enabled'): 
             environment['runtime']['outputManager'].presentText(environment, "speech enabled",soundIconName='SpeechOn', interrupt=True)                 
