629fa3576cbd6a02e5c748c32b8fca0ffebadb0f
chrys <chrys87@web.de>
2016-08-08 11:15:47 +0200

    add toggle_autoread, typo

---
+++ src/fenrir-package/commands/commands/toggle_autoRead.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+class command():
+    def __init__(self):
+        pass
+    def run(self, environment):
+        
+        environment = environment['runtime']['settingsManager'].setSetting(environment, 'speech', 'autoReadIncomming', str(not environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'autoReadIncomming')))   
+        if environment['runtime']['settingsManager'].getSettingAsBool(environment, 'speech', 'autoReadIncomming'): 
+            environment['runtime']['outputManager'].presentText(environment, "autoread enabled",soundIconName='', interrupt=True)
+        else:
+            environment['runtime']['outputManager'].presentText(environment, "autoread disabled",soundIconName='', interrupt=True)                          
+        return environment    
+    def setCallback(self, callback):
+        pass
+    def shutdown(self):
+        pass
