dec9f7a6210c32d791873f2b1e3369443c0d90df
chrys <chrys87@web.de>
2016-08-10 17:54:02 +0200

    add last_incomming and improve other commands

--- src/fenrir-package/commands/commands/last_incomming.py
+++ src/fenrir-package/commands/commands/last_incomming.py
@@ -4,10 +4,10 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
-        if environment['screenData']['newContentText'].strip(" \t\n") == '':
+        if environment['screenData']['newDelta'].strip(" \t\n") == '':
             environment['runtime']['outputManager'].presentText(environment, "blank", interrupt=True)
         else:    
-           environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newContentText'],interrupt=True)
+           environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newDelta'], interrupt=True)
         return environment    
     def setCallback(self, callback):
         pass
