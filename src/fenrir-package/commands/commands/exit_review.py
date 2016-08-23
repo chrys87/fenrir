b948fccb46ec3522026abb45e79b15da9a989a17
chrys87 <chrys87@web.de>
2016-08-23 13:07:04 +0200

    Update exit_review.py

--- src/fenrir-package/commands/commands/exit_review.py
+++ src/fenrir-package/commands/commands/exit_review.py
@@ -4,8 +4,8 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
-        environment['screenData']['oldCursorReview'] = {'x':-1,'y':-1}
-        environment['screenData']['newCursorReview'] = {'x':-1,'y':-1}
+        environment['screenData']['oldCursorReview'] = None
+        environment['screenData']['newCursorReview'] = None
         environment['runtime']['outputManager'].presentText(environment, "leve review mode", interrupt=True)
         return environment    
     def setCallback(self, callback):
