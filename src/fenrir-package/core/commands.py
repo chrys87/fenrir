c9200248eb96889224e8a19abb1b93d18c080f39
chrys87 <chrys87@web.de>
2016-08-23 17:30:31 +0200

    Update commands.py

--- src/fenrir-package/core/commands.py
+++ src/fenrir-package/core/commands.py
@@ -7,7 +7,7 @@ import time
 commandBuffer = {
 'clipboard':['chrys\n', 'test', 'ls\n'],
 'currClipboard': 0,
-'clipboardMark':{'1':None, '2':None}
+'Marks':{'1':None, '2':None}
 }
 
 # used by the commandManager
