d2ea8c120af18b0f231899c339c552612dc24f0a
chrys87 <chrys87@web.de>
2016-08-23 17:19:45 +0200

    Update copy_marked_to_clipboard.py

--- src/fenrir-package/commands/commands/copy_marked_to_clipboard.py
+++ src/fenrir-package/commands/commands/copy_marked_to_clipboard.py
@@ -1,1 +1,16 @@
+#!/bin/python
 
+class command():
+    def __init__(self):
+        pass
+    def run(self, environment):
+        if len(environment['commandBuffer']['clipboard']) == 0:
+            environment['runtime']['outputManager'].presentText(environment, 'clipboard empty', interrupt=True)
+            return environment 
+        environment['commandBuffer']['currClipboard'] = 0
+        environment['runtime']['outputManager'].presentText(environment, environment['commandBuffer']['clipboard'][environment['commandBuffer']['currClipboard']], interrupt=True)
+        return environment                
+    def setCallback(self, callback):
+        pass
+    def shutdown(self):
+pass
