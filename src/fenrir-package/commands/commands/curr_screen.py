557dfb3f1092274dcdff8aa8d0a21846d77b48fe
chrys87 <chrys87@web.de>
2016-08-25 10:42:34 +0200

    Update curr_screen.py

--- src/fenrir-package/commands/commands/curr_screen.py
+++ src/fenrir-package/commands/commands/curr_screen.py
@@ -4,7 +4,7 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
-        if environment['screenData']['newContentText'].strip(" \t\n") == '':
+        if environment['screenData']['newContentText'].strip() == '':
             environment['runtime']['outputManager'].presentText(environment, "screen is empty", soundIcon='EmptyLine', interrupt=True)
         else:    
            environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newContentText'],interrupt=True)
