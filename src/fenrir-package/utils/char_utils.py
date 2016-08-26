d46b37b4e1259f6b8b35b382d4ee8c845fa34bf8
chrys87 <chrys87@web.de>
2016-08-26 10:41:37 +0200

    Update char_utils.py

--- src/fenrir-package/utils/char_utils.py
+++ src/fenrir-package/utils/char_utils.py
@@ -36,3 +36,19 @@ def getNextChar(currX,currY, currText):
         x += 1    
     currChar = wrappedLines[y][x]            
     return x, y, currChar
+
+def getPhonetic(currChar):
+    if len(currChar) != 1:
+        return currChar
+    phoneticsDict = {
+    "A":"alpha", "B":"bravo", "C":"charlie", "D":"delta", "E":"echo",
+    "F":"foxtrot", "G":"golf", "H":"hotel", "I":"india", "J":"juliet",
+    "K":"kilo", "L":"lima", "M":"mike", "N":"november", "O":"oscar",
+    "P":"papa", "Q":"quebec", "R":"romeo", "S":"sierra", "T":"tango",
+    "U":"uniform", "V":"victor", "W":"whisky", "X":"x ray",
+    "Y":"yankee", "Z":"zulu"
+    }
+    try:
+        return phoneticsDict[currChar.upper()]
+    except:
+        return currChar
