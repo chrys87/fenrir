9ce32b484c79afed1c428513d8410d5ea8729c2a
chrys87 <chrys87@web.de>
2016-08-26 12:02:55 +0200

    Update 50000-char_echo.py

--- src/fenrir-package/commands/onScreenChanged/50000-char_echo.py
+++ src/fenrir-package/commands/onScreenChanged/50000-char_echo.py
@@ -17,7 +17,7 @@ class command():
         if environment['screenData']['newDelta'] == '':
             return environment
         # big changes are no char (but the value is bigger than one maybe the differ needs longer than you can type, so a little strange random buffer for now)
-        if len(environment['screenData']['newDelta']) > 8:
+        if len(environment['screenData']['newDelta']) > 3:
             return environment        
         environment['runtime']['outputManager'].presentText(environment, environment['screenData']['newDelta'], interrupt=True)
 
