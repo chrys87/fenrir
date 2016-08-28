3f9880097c3ef97323d37fbd88b1f1b1563155ff
chrys <chrys87@web.de>
2016-08-28 21:58:51 +0200

    improve output of exit_review

--- src/fenrir-package/commands/commands/exit_review.py
+++ src/fenrir-package/commands/commands/exit_review.py
@@ -4,6 +4,11 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
+        if (environment['screenData']['oldCursorReview'] == None) and \
+          (environment['screenData']['newCursorReview'] == None):
+            environment['runtime']['outputManager'].presentText(environment, "Not in review mode", interrupt=True)
+            return environment    
+
         environment['screenData']['oldCursorReview'] = None
         environment['screenData']['newCursorReview'] = None
         environment['runtime']['outputManager'].presentText(environment, "leve review mode", interrupt=True)
