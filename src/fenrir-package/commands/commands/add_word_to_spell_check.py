34d367612d2ce3d27158c23176937744ad0769a5
chrys <chrys87@web.de>
2016-09-06 23:26:55 +0200

    add command for adding word to spellcheck dict

---
+++ src/fenrir-package/commands/commands/add_word_to_spell_check.py
@@ -0,0 +1,49 @@
+#!/bin/python
+from utils import word_utils
+initialized = False
+try:
+    import enchant
+    initialized = True
+except:
+    pass
+    
+class command():
+    def __init__(self):
+        self.language = ''
+        self.spellChecker = None
+    
+    def run(self, environment):
+        if not initialized:
+           environment['runtime']['outputManager'].presentText(environment, 'pychant is not installed', interrupt=True) 
+           return environment
+        if environment['runtime']['settingsManager'].getSetting(environment, 'general', 'spellCheckLanguage') != self.language:
+            try:
+                self.spellChecker = enchant.Dict(environment['runtime']['settingsManager'].getSetting(environment, 'general', 'spellCheckLanguage'))
+                self.language = environment['runtime']['settingsManager'].getSetting(environment, 'general', 'spellCheckLanguage')
+            except:
+                return environment    
+
+        if (environment['screenData']['newCursorReview'] != None):
+            cursorPos = environment['screenData']['newCursorReview'].copy()
+        else:
+            cursorPos = environment['screenData']['newCursor'].copy()
+            
+        # get the word
+        newContent = environment['screenData']['newContentText'].split('\n')[cursorPos['y']]
+        x, y, currWord =  word_utils.getCurrentWord(cursorPos['x'], 0, newContent)                  
+
+        if currWord != '':
+            if self.spellChecker.check(currWord):
+                environment['runtime']['outputManager'].presentText(environment, 'not misspelled',soundIcon='mispell', interrupt=True)
+            else:
+                if self.spellChecker.is_added(currWord):
+                    environment['runtime']['outputManager'].presentText(environment, 'not misspelled',soundIcon='Cancel', interrupt=True)                
+                else:
+                    self.spellChecker.add(currWord)             
+                    environment['runtime']['outputManager'].presentText(environment, currWord + ' added',soundIcon='Accept', interrupt=True)                
+                    
+        return environment
+    def setCallback(self, callback):
+        pass
+    def shutdown(self):
+        pass
