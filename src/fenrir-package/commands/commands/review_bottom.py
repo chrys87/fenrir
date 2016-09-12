5703bb9e7cec51e5422013aece6bbeb391b41aea
chrys <chrys87@web.de>
2016-09-12 21:53:33 +0200

    fix review bottom

--- src/fenrir-package/commands/commands/review_bottom.py
+++ src/fenrir-package/commands/commands/review_bottom.py
@@ -4,9 +4,7 @@ class command():
     def __init__(self):
         pass
     def run(self, environment):
-        environment['screenData']['newCursorReview'] = {}
-        environment['screenData']['newCursorReview']['x'] = environment['screenData']['lines']
-        environment['screenData']['newCursorReview']['y'] = 0
+        environment['screenData']['newCursorReview'] = { 'x': 0, 'y':environment['screenData']['lines']}
         environment['runtime']['outputManager'].presentText(environment, "Bottom", interrupt=True)
         return environment    
     def setCallback(self, callback):
