62630a06acfe66a0e8a7c0219a0392cacaa1cf68
chrys87 <chrys87@web.de>
2016-09-16 12:50:07 +0200

    Update sox.py

--- src/fenrir-package/soundDriver/sox.py
+++ src/fenrir-package/soundDriver/sox.py
@@ -3,17 +3,24 @@ import subprocess
 
 class driver():
     def __init__(self):
-        self.volume = 1.0;
+        self.proc = None
+        self.volume = 1.0
+        self.soundType = ''
     def initialize(self, environment):
         return environment
     def shutdown(self, environment):
         return environment
     def playFrequence(self, frequence, duration, adjustVolume):
-        pass
+        self.soundType = 'frequence'
     def playSoundFile(self, filePath, interrupt = True):
+        if interrupt:
+            self.cancel()
         self.proc = subprocess.Popen("play -q -v " + str(self.volume ) + ' ' + filePath, shell=True)
+        self.soundType = 'file'
     def cancel(self):
-        pass        
+        if self.soundType == 'file':
+            self.proc.kill()
+        self.soundType = ''
     def setCallback(self, callback):
         pass
     def setVolume(self, volume):
