0cb7ea4410be9c64de6226238838c79662c5ebe3
chrys87 <chrys87@web.de>
2016-09-22 16:06:59 +0200

    Update punctuationManager.py

--- src/fenrir-package/core/punctuationManager.py
+++ src/fenrir-package/core/punctuationManager.py
@@ -11,5 +11,46 @@ class punctuationManager():
         pass
     def initialize(self, environment):
         self.env = environment
+        self.punctuation = {
+        'currLevel':'1',
+        'levels':{
+          '1':',',
+          '2':'.',
+          '3':'.,',
+          },
+        'punctuationDict':{
+          '.':'punkt',
+          ',':'komma'
+          },
+        'customDict':{
+          'chrys':'awsome',
+          'cool':'mr chrys'
+          }          
+        }
     def shutdown(self):
         pass
+    def removeUnused(self, text):
+        resultText = text.translate(text.maketrans(string.punctuation, ' '*len(string.punctuation)))   
+        return resultText
+    def useCustomDict(self, text, customDict):
+        resultText = text
+        for key,item in customDict.items():
+            resultText = resultText.replace(str(key),str(item))
+        return resultText
+    def usePunctuationDict(self, text, punctuationDict, punctuation):
+        resultText = str(text)
+        for key,item in punctuationDict.items():
+            if key in punctuation:
+                resultText = resultText.replace(str(key),' ' +str(item) +' ')
+        return resultText
+    
+    def proceedPunctuation(self, text, ignoreLevel=False):
+        resultText = self.useCustomDict(text, self.punctuation['customDict'])
+        currPunctLevel = ''
+        if not ignoreLevel:
+            currPunctLevel = self.punctuation['levels'][self.punctuation['currLevel']]
+        else:
+            currPunctLevel = string.punctuation
+        resultText = self.usePunctuationDict(resultText, self.punctuation['punctuationDict'], currPunctLevel)
+        resultText = self.removeUnused(resultText)
+        return resultText
