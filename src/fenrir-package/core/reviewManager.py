a45489335f0997dd8ba28d1b48041841bcb48e28
chrys87 <chrys87@web.de>
2016-09-22 14:44:32 +0200

    Create reviewManager.py

---
+++ src/fenrir-package/core/reviewManager.py
@@ -0,0 +1,16 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+
+class reviewManager():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+        
