424a1fc5351392f872ac600a50d0f58236af1ca5
chrys87 <chrys87@web.de>
2016-09-23 09:35:22 +0200

    Create applicationManager.py

---
+++ src/fenrir-package/core/applicationManager.py
@@ -0,0 +1,21 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+
+class applicationManager():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def getCurrentApplication(self):
+        pass
+    def getPrevApplication(self):
+        pass
+    def isApplicationChange(self):
+        pass
