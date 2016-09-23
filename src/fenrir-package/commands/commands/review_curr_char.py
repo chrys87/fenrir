a0e9e58f1e53db7577eb0ccd3d63c5ca8c31c115
chrys87 <chrys87@web.de>
2016-09-23 13:25:59 +0200

    Update review_curr_char.py

--- src/fenrir-package/commands/commands/review_curr_char.py
+++ src/fenrir-package/commands/commands/review_curr_char.py
@@ -18,9 +18,7 @@ class command():
         return 'presents the current character.'        
     
     def run(self):
-        self.env['screenData']['oldCursorReview'] = self.env['screenData']['newCursorReview']
-        if not self.env['screenData']['newCursorReview']:
-            self.env['screenData']['newCursorReview'] = self.env['screenData']['newCursor'].copy()
+        self.env['runtime']['cursorManager'].enterReviewModeCurrTextCursor()
 
         self.env['screenData']['newCursorReview']['x'], self.env['screenData']['newCursorReview']['y'], currChar = \
           char_utils.getCurrentChar(self.env['screenData']['newCursorReview']['x'], self.env['screenData']['newCursorReview']['y'], self.env['screenData']['newContentText'])
