47033d9f40dde7066aa9a611e552afe1b4305e57
chrys <chrys87@web.de>
2016-09-25 15:20:47 +0200

    initial single press integration

---
+++ src/fenrir-package/commands/onSwitchApplicationProfile/deactive/agetty.py
@@ -0,0 +1,33 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def getDescription(self):
+        return 'No description found'         
+    def load(self):
+        print('--------------')
+        print('agetty')    
+        print('load old',self.env['screenData']['oldApplication'])
+        print('load new',self.env['screenData']['newApplication'])        
+        print('--------------')
+        
+    def unload(self):
+        print('--------------')
+        print('agetty')    
+        print('unload old',self.env['screenData']['oldApplication'])
+        print('unload new',self.env['screenData']['newApplication'])      
+        print('--------------')
+          
+    def setCallback(self, callback):
+        pass
