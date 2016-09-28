d409bd3e990835e4abb0e09f884b6624abc834b3
Jeremiah Ticket <jticket1024@users.noreply.github.com>
2016-09-28 13:24:20 -0800

    Corrected some grammar in the text and return.

--- src/fenrir-package/commands/commands/toggle_tutorial_mode.py
+++ src/fenrir-package/commands/commands/toggle_tutorial_mode.py
@@ -18,7 +18,7 @@ class command():
         return 'You are leving the tutorial mode. Press that shortcut again to enter the tutorial mode again.'        
     
     def run(self):
-        text = 'you entered the tutorial mode. In that mode the commands are not executed. but you get an description what the shortcut does. to leve the tutorial mode press that shortcut again.'
+        text = 'you entered the tutorial mode. In that mode the commands are not executed. but you get a description of what the shortcut does. To leve the tutorial mode, press that shortcut again.'
         self.env['runtime']['outputManager'].presentText(text,  interrupt=True)                  
         self.env['generalInformation']['tutorialMode'] = True            
     
