f384752a5da2fdb304c19933d1ed6a5b11ce9f31
chrys <chrys87@web.de>
2016-09-28 22:06:10 +0200

    fix clipboard

--- src/fenrir-package/commands/commands/copy_marked_to_clipboard.py
+++ src/fenrir-package/commands/commands/copy_marked_to_clipboard.py
@@ -22,14 +22,13 @@ class command():
           self.env['commandBuffer']['Marks']['2']):
             self.env['runtime']['outputManager'].presentText("two marks needed", interrupt=True)
             return
-
         # use the last first and the last setted mark as range
         startMark = self.env['commandBuffer']['Marks']['1'].copy()
         endMark = self.env['commandBuffer']['Marks']['2'].copy()         
         
         marked = mark_utils.getTextBetweenMarks(startMark, endMark, self.env['screenData']['newContentText'])
 
-        self.env['commandBuffer']['clipboard'] = [marked] + self.env['commandBuffer']['clipboard'][:self.env['runtime']['settingsManager'].getSettingAsFloat('general', 'numberOfClipboards') -1]
+        self.env['commandBuffer']['clipboard'] = [marked] + self.env['commandBuffer']['clipboard'][:self.env['runtime']['settingsManager'].getSettingAsInt('general', 'numberOfClipboards') -1]
         self.env['commandBuffer']['currClipboard'] = 0
         # reset marks
         self.env['commandBuffer']['Marks']['1'] = None
