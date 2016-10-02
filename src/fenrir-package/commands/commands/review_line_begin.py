3eb5fb0f478bf63eb9be58f31648a22f796b8ca2
chrys <chrys87@web.de>
2016-10-02 16:25:53 +0200

    name line begin/end commands more consistent

---
+++ src/fenrir-package/commands/commands/review_line_begin.py
@@ -0,0 +1,25 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass 
+    def getDescription(self):
+        return 'displays the position of the review cursor'        
+
+    def run(self):
+        cursorPos = self.env['runtime']['cursorManager'].getReviewOrTextCursor()
+        self.env['runtime']['cursorManager'].setReviewCursorPosition(0 ,cursorPos['y'])
+        self.env['runtime']['outputManager'].presentText("begin of line", interrupt=True)
+   
+    def setCallback(self, callback):
+        pass
