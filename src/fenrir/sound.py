e8032a3b308d4586999ee3145a973749cfadcf22
Jeremiah Ticket <jticket1024@users.noreply.github.com>
2016-10-04 11:18:09 -0800

    Revert "Renamed fenrir-package to fenrir to satisfy packaging deps."
    This reverts commit 33edce7c828ef7756a0611dcbfed721853cc9b9c.

--- src/fenrir/sound.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-
-import time
-
-from sound.gstreamer import sound
-
-s = sound()
-s.playSoundFile('/home/chrys/Projekte/fenrir/fenrir/src/fenrir-package/1ChangeTTY.opus')
-time.sleep(10)
--- src/fenrir/utils/review_utils.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
-
-# Fenrir TTY screen reader
-# By Chrys, Storm Dragon, and contributers.
-
-from core import debug
-
