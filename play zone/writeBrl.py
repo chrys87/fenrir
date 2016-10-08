0ccf22e378b67037299114ca81d5fca4e6bf8942
chrys <chrys87@web.de>
2016-10-08 02:37:09 +0200

    replace loop by an sleep

--- play zone/writeBrl.py
+++ play zone/writeBrl.py
@@ -9,10 +9,10 @@ print('display size' + str(brl.displaySize))
 print('driver name'+str(brl.driverName))
 
 t = time.time()
-while(time.time() - t <= 5):
-    try:
-        brl.writeText(  'this is a 5 second test')
-    except Exception as e:
-        print(e)
+#while(time.time() - t <= 5):
+try:
+    brl.writeText(  'this is a 5 second test')
+except Exception as e:
+    print(e)
 brl.leaveTtyMode()    
-
+time.sleep(5)
