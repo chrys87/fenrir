fd4a6cdcbe4dd952fe2d90cce23ed79b9078e774
chrys <chrys87@web.de>
2016-10-08 02:08:01 +0200

    add simple braille demo

---
+++ play zone/writeBrl.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import brlapi
+
+brl =  = brlapi.Connection()
+print(brl.displaySize)
+print(brl.driverName)
+try:
+    brl.writeText('test')
+except Exception as e:
+    print(e)
+    
+
