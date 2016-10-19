60d4a74a751768cb6196aa10f5d95d5aabb0a74e
chrys <chrys87@web.de>
2016-10-19 23:56:53 +0200

    cleanup move demo application to play zone

---
+++ play zone/daemon.py
@@ -0,0 +1,12 @@
+from time import sleep
+from daemonize import Daemonize
+
+pid = "/tmp/test.pid"
+
+
+def main():
+    while True:
+        sleep(5)
+
+daemon = Daemonize(app="test_app", pid=pid, action=main)
+daemon.start()
