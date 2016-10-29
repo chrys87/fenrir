5839a8fbcbdf68bf203a3b82f7aed97f0875a13d
chrys <chrys87@web.de>
2016-10-29 16:40:34 +0200

    create screen_utils

---
+++ src/fenrir/utils/screen_utils.py
@@ -0,0 +1,52 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+from collections import Counter
+
+def insertNewlines(string, every=64):
+    return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
+
+def splitAtrrLines(string, every=64):
+    return list(string[i:i+every] for i in range(0, len(string), every))
+
+old = b'eeeemmmeeeeeeeee'
+new = b'eeeeeueeeeeeeeee'
+text = 'das ist ein test'
+
+def trackHighlights(oldAttr, newAttr, text):
+    result = ''
+    currCursor = None
+    if oldAttr == newAttr:
+        return result,  currCursor
+    if len(newAttr) == 0:
+        return result,  currCursor
+    textLines = insertNewlines(text,4)
+    textLines = textLines.split('\n')    
+    old = splitAtrrLines(oldAttr,4)
+    new = splitAtrrLines(newAttr,4)    
+    if len(old) != len(new):
+        return result,  currCursor
+    if len(text) != len(new):
+        return result,  currCursor
+    try:
+        background = Counter(newAttr).most_common(1)
+        background = background[0][0]
+    except Exception as e:
+        background = chr(7)
+    for line in range(len(new)):
+        if old[line] != new[line]:
+            for column in range(len(new)):
+                if old[line][column] != new[line][column]:
+                    if new[line][column] != background:
+                        if not currCursor:
+                            currCursor['x'] = column
+                            currCursor['y'] = line
+                        result += textLines[line][column]
+            result += ' '
+    return result, currCursor
+
+print(trackHighlights(alts,neus,text))    
