e06bd164237a5cd6ea145c5a3740c025e99e8977
chrys <chrys87@web.de>
2016-11-04 22:24:15 +0100

    add command for toggle highlight tracking

---
+++ src/fenrir/commands/onInput/56000-highlight_tracking.py
@@ -0,0 +1,24 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass 
+    def getDescription(self):
+        return 'enables or disables tracking of highlighted'        
+    
+    def run(self):
+        if not self.env['runtime']['settingsManager'].getSettingAsBool('focus', 'highlight'):
+            return
+        self.env['runtime']['outputManager'].presentText(self.env['screenData']['newAttribDelta'], soundIcon='', interrupt=True)                          
+    
+    def setCallback(self, callback):
+        pass
