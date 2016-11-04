8e26484d3f7ef7f9c65b9cad6ea67893494ab0a9
chrys <chrys87@web.de>
2016-11-04 21:32:35 +0100

    remove wrong intention

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 #Basic install script for fenrir.
 read -p "This will install fenrir. Press ctrl+c to cancil, or enter to continue." continue
 install -m755 -d /opt/fenrir
- install -m755 -d /usr/share/fenrir/scripts
- install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
- install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
- install -m644 -D "config/punctuation/default.conf" /etc/fenrir/punctuation/default.conf
- install -m644 -D "config/settings/settings.conf" /etc/fenrir/settings/settings.conf
- install -d /usr/share/sounds/fenrir
-  install -m644 -D "autostart/systemd/fenrir.service" /usr/lib/systemd/system/fenrir.service
-  cp -a src/fenrir/* /opt/fenrir
-  cp -a config/scripts/* /usr/share/fenrir/scripts
-  cp -a config/sound/* /usr/share/sounds/fenrir
+install -m755 -d /usr/share/fenrir/scripts
+install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
+install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
+install -m644 -D "config/punctuation/default.conf" /etc/fenrir/punctuation/default.conf
+install -m644 -D "config/settings/settings.conf" /etc/fenrir/settings/settings.conf
+install -d /usr/share/sounds/fenrir
+install -m644 -D "autostart/systemd/fenrir.service" /usr/lib/systemd/system/fenrir.service
+cp -a src/fenrir/* /opt/fenrir
+cp -a config/scripts/* /usr/share/fenrir/scripts
+cp -a config/sound/* /usr/share/sounds/fenrir
 ln -s /opt/fenrir/fenrir-daemon /usr/bin/fenrir
 cat << EOF
 To have fenrir start at boot:
