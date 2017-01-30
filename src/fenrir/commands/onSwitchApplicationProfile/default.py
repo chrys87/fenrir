3ee43a3a85174adb003d9361cf7990dd7c3d392a
chrys <chrys87@web.de>
2017-01-30 20:58:47 +0100

    move tests to new folder

--- src/fenrir/commands/onSwitchApplicationProfile/default.py
+++ src/fenrir/commands/onSwitchApplicationProfile/default.py
@@ -16,12 +16,14 @@ class command():
     def getDescription(self):
         return 'No description found'         
     def load(self):
+        return
         print('--------------')
         print('default')    
         print('load new',self.env['screenData']['newApplication'])        
         print('--------------')
         
     def unload(self):
+        return
         print('--------------')
         print('default')    
         print('unload old',self.env['screenData']['oldApplication'])  
