7130735af8b0b14ceca93425a287fac47d01dca5
chrys87 <chrys87@web.de>
2017-02-02 10:14:06 +0100

    Update deinstall.sh

--- deinstall.sh
+++ deinstall.sh
@@ -5,12 +5,12 @@ fenrir is going to remove.
 every script and settings are lost.
 EOF
 read -p "This will remove fenrir and settings. Press ctrl+c to cancil, or enter to continue." continue
+unlink /usr/bin/fenrir
 rm -r /opt/fenrir
 rm -r /usr/share/fenrir
 rm -r /etc/fenrir
 rm -r /usr/share/sounds/fenrir
 
-unlink /usr/bin/fenrir
 cat << EOF
 fenrir is removed
 EOF
