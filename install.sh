edd6779e1c8593991f0e3461046e6cfef3b7cc24
chrys87 <chrys87@web.de>
2017-02-02 09:55:22 +0100

    copy tools and all punctuation in install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,20 @@
 read -p "This will install fenrir. Press ctrl+c to cancil, or enter to continue." continue
 install -m755 -d /opt/fenrir
 install -m755 -d /usr/share/fenrir/scripts
+install -m755 -d /usr/share/fenrir/tools
+install -m755 -d /etc/fenrir/punctuation 
 install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
 install -m644 -D "config/keyboard/desktop.conf" /etc/fenrir/keyboard/desktop.conf
-install -m644 -D "config/punctuation/default.conf" /etc/fenrir/punctuation/default.conf
 install -m644 -D "config/settings/settings.conf" /etc/fenrir/settings/settings.conf
 install -d /usr/share/sounds/fenrir
 install -m644 -D "autostart/systemd/fenrir.service" /usr/lib/systemd/system/fenrir.service
+
 cp -a src/fenrir/* /opt/fenrir
 cp -a config/scripts/* /usr/share/fenrir/scripts
+cp -a config/punctuation/* /etc/fenrir/punctuation 
 cp -a config/sound/* /usr/share/sounds/fenrir
+cp -a tools/* /usr/share/fenrir/tools
+
 ln -s /opt/fenrir/fenrir-daemon /usr/bin/fenrir
 cat << EOF
 To have fenrir start at boot:
