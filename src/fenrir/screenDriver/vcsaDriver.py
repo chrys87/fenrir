728adedbe31350fe306ac7e56bd6b5d89d781705
chrys <chrys87@web.de>
2017-02-13 21:38:57 +0100

    remove unneded delay

--- src/fenrir/screenDriver/vcsaDriver.py
+++ src/fenrir/screenDriver/vcsaDriver.py
@@ -35,7 +35,6 @@ class driver():
         with open(useScreen, 'w') as fd:
             for c in text:
                 fcntl.ioctl(fd, termios.TIOCSTI, c)
-                time.sleep(0.0005)
                 
     def getCurrApplication(self):
         apps = []
