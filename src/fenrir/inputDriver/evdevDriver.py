64e96f3a545dbbae327ba2ebfd4a034f408fa53d
chrys <chrys87@web.de>
2017-05-30 22:52:01 +0200

    try to fix stucking key again

--- src/fenrir/inputDriver/evdevDriver.py
+++ src/fenrir/inputDriver/evdevDriver.py
@@ -92,7 +92,7 @@ class driver():
             return    
         uDevice.write_event(event)
         uDevice.syn()  
-        time.sleep(0.05)
+        time.sleep(0.01)
 
     def updateInputDevices(self, force = False, init = False):
         if init:
