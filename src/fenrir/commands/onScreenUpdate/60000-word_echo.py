8ca676271aad3a68ecf812d0afb12e3b8f93f368
chrys <chrys87@web.de>
2017-06-30 19:37:55 +0200

    move some commands to onScreenUpdate (WIP)

---
+++ src/fenrir/commands/onScreenUpdate/60000-word_echo.py
@@ -0,0 +1,55 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from core import debug
+from utils import word_utils
+import string
+
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def getDescription(self):
+        return 'No Description found'     
+
+    def run(self):
+        # first place could not be the end of a word
+        if self.env['screen']['newCursor']['x'] == 0:
+            return
+        # is it enabled?    
+        if not self.env['runtime']['settingsManager'].getSettingAsBool('keyboard', 'wordEcho'):
+            return
+
+        # just when cursor move worddetection is needed
+        if not self.env['runtime']['cursorManager'].isCursorHorizontalMove():
+            return
+        # for now no new line
+        if self.env['runtime']['cursorManager'].isCursorVerticalMove():
+            return
+        # get the word            
+        newContent = self.env['screen']['newContentText'].split('\n')[self.env['screen']['newCursor']['y']]
+        x, y, currWord, endOfScreen, lineBreak = \
+          word_utils.getCurrentWord(self.env['screen']['newCursor']['x'], 0, newContent)                          
+        # currently writing
+        if self.env['runtime']['screenManager'].isDelta():
+            return
+        else:
+        # at the end of a word
+            if not newContent[self.env['screen']['newCursor']['x']].isspace():
+                return
+            if (x + len(currWord) != self.env['screen']['newCursor']['x']) and \
+              (x + len(currWord) != self.env['screen']['newCursor']['x']-1):
+                return    
+
+        if currWord != '':
+            self.env['runtime']['outputManager'].presentText(currWord, interrupt=True, flush=False)
+
+    def setCallback(self, callback):
+        pass
+
