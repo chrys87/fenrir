32e11ac9a756ac6d16ba8bd4e4b4cc86d756b8c8
chrys87 <chrys87@web.de>
2017-07-24 15:35:27 +0200

    Update evdevDriver.py

--- src/fenrir/inputDriver/evdevDriver.py
+++ src/fenrir/inputDriver/evdevDriver.py
@@ -147,7 +147,7 @@ class driver():
             return    
         uDevice.write_event(event)
         uDevice.syn()  
-        time.sleep(0.0001)
+        time.sleep(0.00001)
 
     def updateInputDevices(self, force = False, init = False):
         if init:
