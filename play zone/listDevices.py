f83ea851525c9d2f8d1e05a1ce28e0502b1e876c
chrys <chrys87@web.de>
2017-10-01 11:30:35 +0200

    add better debug tool for input devices

--- play zone/listDevices.py
+++ play zone/listDevices.py
@@ -8,6 +8,15 @@ iDevices = map(evdev.InputDevice, (evdev.list_devices()))
 iDevices = {dev.fd: dev for dev in iDevices}
 
 for fd in iDevices:
+    print('----------------------')
     dev = iDevices[fd]
     cap = dev.capabilities()
-    print(dev.name,dev.leds(),'has keys:'+str(evdev.events.EV_KEY in cap),'is mouse:'+str(((evdev.events.EV_REL in cap) or (evdev.events.EV_ABS in cap))))
+    print('Name: ' + str(dev.name))
+    print('LEDs: ' + str(dev.leds()))
+    print('Has Keys: '+ str(evdev.events.EV_KEY in cap))
+    if evdev.events.EV_KEY in cap:
+        print('No. of keys: ' + str(len(cap[evdev.events.EV_KEY])))
+        print('has Key 116: ' + str(116 in cap[evdev.events.EV_KEY]))
+    print('Is Mouse: ' + str(((evdev.events.EV_REL in cap) or (evdev.events.EV_ABS in cap))))
+    print('----------------------')
+
