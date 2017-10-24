6b3a89a837f1e0d33c7f3577050ea58eef7061da
chrys <chrys87@web.de>
2017-10-24 07:39:02 +0200

    start inherit more drivers

--- src/fenrir/core/inputDriver.py
+++ src/fenrir/core/inputDriver.py
@@ -9,51 +9,39 @@ from core import debug
 class inputDriver():
     def __init__(self):
         self._initialized = False
-        
     def initialize(self, environment):
         self.env = environment
-        self._isInitialized = True        
+        self._isInitialized = True
     def shutdown(self):
-        pass
-        
+        self._isInitialized = False
     def getInputEvent(self):
         time.sleep(0.05)
         if not self._initialized:
             return None
-
     def writeEventBuffer(self):
         if not self._initialized:
             return    
-
     def clearEventBuffer(self):
         if not self._initialized:
             return    
         del self.env['input']['eventBuffer'][:]            
-
     def updateInputDevices(self, force = False, init = False):
         if not self._initialized:
             return    
-       
     def getLedState(self, led = 0):
         if not self._initialized:
             return False    
-        return False          
-
+        return False
     def toggleLedState(self, led = 0):
         if not self._initialized:
-            return None    
-
+            return None
     def grabDevices(self):
         if not self._initialized:
-            return None    
-
+            return None
     def releaseDevices(self):
         if not self._initialized:
-            return None    
-
+            return None
     def __del__(self):
         if not self._initialized:
             return None        
         self.releaseDevices()
-
-
