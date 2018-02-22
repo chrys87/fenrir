86960693df59b0601f8455e611ac0178154b8224
chrys <chrys87@web.de>
2018-02-22 20:18:17 +0100

    wip api rework

--- src/fenrir/core/inputManager.py
+++ src/fenrir/core/inputManager.py
@@ -10,67 +10,99 @@ from core import debug
 class inputManager():
     def __init__(self):
         self.setLedState = True
+        self.currInput = []
+        self.prevDeepestInput = []
+        self.eventBuffer = []
+        self.shortcutRepeat = 0
+        self.fenrirKey = []
+        self.scriptKey = []
+        self.keyForeward = 0
+        self.lastInputTime = time.time()
+        self.lastDeepestInput = []
+        self.NumLock = {'prev': True, 'current': True }
+        self.ScrollLock = {'prev': True, 'current': True }
+        self.CapsLock = {'prev': False, 'current': False }
+    def setCurrInput(self, value):
+        if len(self.getCurrInput()) > 1:
+            self.currInput = sorted(self.getCurrInput())  
+    def getCurrInput(self):
+        return self.currInput
+    def addToCurrInput(self, value):
+        currInput = self.getCurrInput().append(value)
+        if len(currInput) > 1:
+            currInput = sorted(currInput)                
+        self.setCurrInput(currInput)
+    def removeFromCurrInput(self, value):        
+        currInput = self.getCurrInput().remove(value)
+        if len(currInput) > 1:
+            currInput = sorted(currInput)        
+        self.setCurrInput(currInput)
+    def increaseShortcutRepeat(self):
+        self.shortcutRepeat += 1    
+    def resetShortcutRepeat(self):
+        self.shortcutRepeat = 1
+    def getShortcutRepeat(self):
+        return self.shortcutRepeat
     def initialize(self, environment):
         self.env = environment
         self.env['runtime']['settingsManager'].loadDriver(\
           self.env['runtime']['settingsManager'].getSetting('keyboard', 'driver'), 'inputDriver')
         self.updateInputDevices()
         # init LEDs with current state
-        self.env['input']['newNumLock'] = self.env['runtime']['inputDriver'].getLedState()
-        self.env['input']['oldNumLock'] = self.env['input']['newNumLock']
-        self.env['input']['newCapsLock'] = self.env['runtime']['inputDriver'].getLedState(1)
-        self.env['input']['oldCapsLock'] = self.env['input']['newCapsLock']
-        self.env['input']['newScrollLock'] = self.env['runtime']['inputDriver'].getLedState(2)
-        self.env['input']['oldScrollLock'] = self.env['input']['newScrollLock']
-        self.lastDeepestInput = []
-        self.lastInputTime = time.time()
+        self.setNumLock(self.env['runtime']['inputDriver'].getLedState())
+        self.setNumLock(self.getNumLock())
+        self.setCapsLock(self.env['runtime']['inputDriver'].getLedState(1))
+        self.setCapsLock(self.getCapsLock())
+        self.setScrollLock(self.env['runtime']['inputDriver'].getLedState(2))
+        self.setScrollLock(self.getScrollLock())
     def shutdown(self):
         self.removeAllDevices()
         self.env['runtime']['settingsManager'].shutdownDriver('inputDriver')
-
+    def newMappedInputEvent(self):
+        return {
+        'EventName': '',
+        'EventValue': '',
+        'EventSec': 0,
+        'EventUsec': 0,
+        'EventState': 0,
+        }    
     def getInputEvent(self):
+        return self.env['runtime']['inputDriver'].getInputEvent()
+    def handleInputEvent(self, mEvent = None):
         eventReceived = False
-        mEvent = self.env['runtime']['inputDriver'].getInputEvent()
         if mEvent:
             mEvent['EventName'] = self.convertEventName(mEvent['EventName'])        
             eventReceived = True
-            self.env['input']['prevInput'] = self.env['input']['currInput'].copy()
+            self.prevInput = self.getCurrInput().copy()
             if mEvent['EventState'] == 0:
-                if mEvent['EventName'] in self.env['input']['currInput']:
-                    self.env['input']['currInput'].remove(mEvent['EventName'])
-                    if len(self.env['input']['currInput']) > 1:
-                        self.env['input']['currInput'] = sorted(self.env['input']['currInput'])
-                    elif len(self.env['input']['currInput']) == 0:
+                if mEvent['EventName'] in self.getCurrInput():
+                    self.removeFromCurrInput(mEvent['EventName'])
+                    elif len(self.getCurrInput()) == 0:
                         self.env['input']['shortcutRepeat'] = 1 
                     self.setLedState = self.handleLedStates(mEvent)                                             
                     self.lastInputTime = time.time()                                                   
             elif mEvent['EventState'] == 1:
-                if not mEvent['EventName'] in self.env['input']['currInput']:
-                    self.env['input']['currInput'].append(mEvent['EventName'])
-                    if len(self.env['input']['currInput']) > 1:
-                        self.env['input']['currInput'] = sorted(self.env['input']['currInput'])
-                    if len(self.lastDeepestInput) < len(self.env['input']['currInput']):
-                        self.setLastDeepestInput( self.env['input']['currInput'].copy())
-                    elif self.lastDeepestInput == self.env['input']['currInput']:
+                if not mEvent['EventName'] in self.getCurrInput():
+                    self.addToCurrInput(mEvent['EventName'])
+                    if len(self.lastDeepestInput) < len(self.getCurrInput()):
+                        self.setLastDeepestInput( self.getCurrInput().copy())
+                    elif self.lastDeepestInput == self.getCurrInput():
                         if time.time() - self.lastInputTime <= self.env['runtime']['settingsManager'].getSettingAsFloat('keyboard','doubleTapTimeout'):
-                            self.env['input']['shortcutRepeat'] += 1
+                            self.increaseShortcutRepeat()
                         else:
-                            self.env['input']['shortcutRepeat'] = 1
+                            self.resetShortcutRepeat()
                     self.setLedState = self.handleLedStates(mEvent)                                             
                     self.lastInputTime = time.time()                                               
             elif mEvent['EventState'] == 2:
                 self.lastInputTime  = time.time()                                                   
             else:
                 pass  
-            self.env['input']['oldNumLock'] = self.env['input']['newNumLock']
-            self.env['input']['newNumLock'] = self.env['runtime']['inputDriver'].getLedState()
-            self.env['input']['oldCapsLock'] = self.env['input']['newCapsLock'] 
-            self.env['input']['newCapsLock'] = self.env['runtime']['inputDriver'].getLedState(1)
-            self.env['input']['oldScrollLock'] = self.env['input']['newScrollLock'] 
-            self.env['input']['newScrollLock'] = self.env['runtime']['inputDriver'].getLedState(2)
-            self.env['runtime']['debug'].writeDebugOut("currInput " + str(self.env['input']['currInput'] ) ,debug.debugLevel.INFO)              
+            self.setScrollLock(self.env['runtime']['inputDriver'].getLedState(2))
+            self.setNumLock(self.env['runtime']['inputDriver'].getLedState())
+            self.setCapsLock(self.env['runtime']['inputDriver'].getLedState(1))
+            self.env['runtime']['debug'].writeDebugOut("currInput " + str(self.getCurrInput() ) ,debug.debugLevel.INFO)              
             if self.noKeyPressed():
-                self.env['input']['prevInput'] = []
+                self.prevInput = []
                 self.setLedState = True
         return eventReceived
 
@@ -78,24 +110,24 @@ class inputManager():
         if not self.setLedState:
             return self.setLedState
         if mEvent['EventName'] == 'KEY_NUMLOCK':
-            if mEvent['EventState'] == 1 and not self.env['input']['newNumLock'] == 1:
+            if mEvent['EventState'] == 1 and not self.getNumLock() == 1:
                 self.env['runtime']['inputDriver'].toggleLedState() 
                 return False
-            if mEvent['EventState'] == 0 and not self.env['input']['newNumLock'] == 0:
+            if mEvent['EventState'] == 0 and not self.getNumLock() == 0:
                 self.env['runtime']['inputDriver'].toggleLedState()                                                                        
                 return False
         if mEvent['EventName'] == 'KEY_CAPSLOCK':   
-            if mEvent['EventState'] == 1 and not self.env['input']['newCapsLock'] == 1:
+            if mEvent['EventState'] == 1 and not self.getCapsLock() == 1:
                 self.env['runtime']['inputDriver'].toggleLedState(1)              
                 return False
-            if mEvent['EventState'] == 0 and not self.env['input']['newCapsLock'] == 0:
+            if mEvent['EventState'] == 0 and not self.getCapsLock() == 0:
                 self.env['runtime']['inputDriver'].toggleLedState(1)                                                                         
                 return False                                      
         if mEvent['EventName'] == 'KEY_SCROLLLOCK':  
-            if mEvent['EventState'] == 1 and not self.env['input']['newScrollLock'] == 1:
+            if mEvent['EventState'] == 1 and not self.getScrollLock() == 1:
                 self.env['runtime']['inputDriver'].toggleLedState(2)              
                 return False
-            if mEvent['EventState'] == 0 and not self.env['input']['newScrollLock'] == 0:
+            if mEvent['EventState'] == 0 and not self.getScrollLock() == 0:
                 self.env['runtime']['inputDriver'].toggleLedState(2)                                                                       
                 return False
         return self.setLedState
@@ -181,24 +213,24 @@ class inputManager():
 
     def getCurrShortcut(self, inputSequence = None):
         shortcut = []
-        shortcut.append(self.env['input']['shortcutRepeat'])
+        shortcut.append(self.getShortcutRepeat())
         if inputSequence:
             shortcut.append(inputSequence)
         else:        
-            shortcut.append(self.env['input']['currInput'])
-        if len(self.env['input']['prevInput']) < len(self.env['input']['currInput']):
-            if self.env['input']['shortcutRepeat'] > 1  and not self.shortcutExists(str(shortcut)):
+            shortcut.append(self.getCurrInput())
+        if len(self.env['input']['prevInput']) < len(self.getCurrInput()):
+            if self.getShortcutRepeat() > 1  and not self.shortcutExists(str(shortcut)):
                 shortcut = []
-                self.env['input']['shortcutRepeat'] = 1
+                self.resetShortcutRepeat()
                 shortcut.append(self.env['input']['shortcutRepeat'])
-                shortcut.append(self.env['input']['currInput'])     
+                shortcut.append(self.getCurrInput())     
         self.env['runtime']['debug'].writeDebugOut("currShortcut " + str(shortcut) ,debug.debugLevel.INFO)                      
         return str(shortcut)
         
     def currKeyIsModifier(self):
         if len(self.getLastDeepestInput()) != 1:
             return False
-        return (self.env['input']['currInput'][0] =='KEY_FENRIR') or (self.env['input']['currInput'][0] == 'KEY_SCRIPT')
+        return (self.getCurrInput()[0] =='KEY_FENRIR') or (self.getCurrInput()[0] == 'KEY_SCRIPT')
     
     def isFenrirKey(self, eventName):
         return eventName in self.env['input']['fenrirKey']
@@ -213,3 +245,19 @@ class inputManager():
 
     def shortcutExists(self, shortcut):
         return(shortcut in self.env['bindings'])
+    def setNumLock(self, value):
+        self.NumLock['prev'] = self.NumLock['current']
+        self.NumLock['current'] = value
+    def setScrollLock(self, value):
+        self.ScrollLock['prev'] = self.ScrollLock['current']
+        self.ScrollLock['current'] = value
+    def setCapsLock(self, value):
+        self.CapsLock['prev'] = self.CapsLock['current']
+        self.CapsLock['current'] = value
+
+    def getNumLock(self, what = 'current'):
+        return self.NumLock[what]
+    def getScrollLock(self, what = 'current'):
+        return self.ScrollLock[what]
+    def getCapsLock(self, what = 'current'):
+        return self.CapsLock[what]        
