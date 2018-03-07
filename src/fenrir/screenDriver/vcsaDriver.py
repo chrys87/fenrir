febb9afe05a20ea1637658ed1a482c4659c8e1b1
chrys87 <chrys87@web.de>
2018-03-07 13:28:49 +0100

    Update vcsaDriver.py

--- src/fenrir/screenDriver/vcsaDriver.py
+++ src/fenrir/screenDriver/vcsaDriver.py
@@ -113,7 +113,7 @@ class driver(screenDriver):
         except Exception as e:
             self.env['runtime']['debug'].writeDebugOut('getSessionInformation: Maybe no LoginD:' + str(e),debug.debugLevel.ERROR)               
             self.env['screen']['autoIgnoreScreens'] = []     
-        self.env['runtime']['debug'].writeDebugOut('getSessionInformation:'  + str(self.env['screen']['autoIgnoreScreens']) + ' ' + str(self.env['general'])  ,debug.debugLevel.INFO)                           
+        #self.env['runtime']['debug'].writeDebugOut('getSessionInformation:'  + str(self.env['screen']['autoIgnoreScreens']) + ' ' + str(self.env['general'])  ,debug.debugLevel.INFO)                           
  
     def updateWatchdog(self,active , eventQueue):
         try:
