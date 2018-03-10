83b3e075f4849df74a2d74eab3b41bb2904ebdff
chrys <chrys87@web.de>
2018-03-10 17:31:31 +0100

    merge bleed

--- src/fenrir/core/tableManager.py
+++ src/fenrir/core/tableManager.py
@@ -9,8 +9,38 @@ import re
 
 class tableManager():
     def __init__(self):
-        pass
+        self.headLine = ''
+        self.defaultSeparators = ['+',';','|',' ']
+        self.noOfHeadLineColumns = 0
+        self.headColumnSep = ''
+        self.rowColumnSep = ''
     def initialize(self, environment):
         self.env = environment  
     def shutdown(self):
         pass
+    def resetTableMode(self):
+        self.setHeadLine()
+    def setHeadColumnSep(self, columnSep = ''):
+        self.headColumnSep = columnSep                
+        if columnSep == '':
+            self.noOfHeadLineColumns = 0        
+        else:
+            self.counNoOfHeadColumns()
+    def counNoOfHeadColumns(self):
+        pass
+    def searchForHeadColumnSep(self, headLine):
+        if ' ' in headLine:
+            return ' '
+        return ''
+    def setRowColumnSep(self, columnSep = ''):
+        self.rowColumnSep = columnSep                
+        
+    def setHeadLine(self, headLine = ''):        
+        self.setHeadColumnSep()
+        self.setRowColumnSep()                    
+        if headLine != '':
+            sep = self.searchForHeadColumnSep(headLine)
+            if sep != '':
+                self.headLine = headLine
+                self.setHeadColumnSep(sep)
+
