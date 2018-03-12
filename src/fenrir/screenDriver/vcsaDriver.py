60c4940a5dbecf628b10831af591e4788681310f
chrys <chrys87@web.de>
2018-03-12 21:54:57 +0100

    Adjust wait

--- src/fenrir/screenDriver/vcsaDriver.py
+++ src/fenrir/screenDriver/vcsaDriver.py
@@ -179,7 +179,7 @@ class driver(screenDriver):
                             screenContent = dirtyContent
                             if time.time() - timeout >= 0.4:
                                 break
-                            time.sleep(0.008)
+                            time.sleep(0.02)
                             vcsa[currScreen].seek(0)                             
                             dirtyContent = vcsa[currScreen].read()
                         eventQueue.put({"Type":fenrirEventType.ScreenUpdate,"Data":screenContent})
