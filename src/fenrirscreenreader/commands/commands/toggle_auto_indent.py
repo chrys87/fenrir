d26730c2b9524820c51ec818243f529619acb89d
chrys <chrys87@web.de>
2018-03-24 20:10:28 +0100

    add toggle for present auto ident

---
+++ src/fenrirscreenreader/commands/commands/toggle_auto_indent.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from fenrirscreenreader.core import debug
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass 
+    def getDescription(self):
+        return _('enables or disables automatic reading of idention level changes')        
+    
+    def run(self):
+        self.env['runtime']['settingsManager'].setSetting('general', 'autoPresentIndent', str(not self.env['runtime']['settingsManager'].getSettingAsBool('general', 'autoPresentIndent')))
+        if self.env['runtime']['settingsManager'].getSettingAsBool('general', 'autoPresentIndent'): 
+            self.env['runtime']['outputManager'].presentText(_("autoident enabled"), soundIcon='', interrupt=True)
+        else:
+            self.env['runtime']['outputManager'].presentText(_("autoident disabled"), soundIcon='', interrupt=True)
+    
+    def setCallback(self, callback):
+        pass
