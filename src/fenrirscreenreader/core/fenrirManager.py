7182bc655629bf38ce86d30dd3411a73ab2a762f
chrys <chrys87@web.de>
2018-03-27 00:36:11 +0200

    add -E

--- src/fenrirscreenreader/core/fenrirManager.py
+++ src/fenrirscreenreader/core/fenrirManager.py
@@ -39,7 +39,7 @@ class fenrirManager():
         parser.add_argument('-o', '--options', metavar='SECTION#SETTING=VALUE,..', default='', help='Overwrite options in given settings file')       
         parser.add_argument('-d', '--debug',  action='store_true', help='Turns on Debugmode') 
         parser.add_argument('-p', '--print',  action='store_true', help='Print debug messages on screen')                                         
-        parser.add_argument('-e', '--emulation',  action='store_true', help='Use PTY emulation and escape sequences for input')                                                 
+        parser.add_argument('-e', '--emulated-pty',  action='store_true', help='Use PTY emulation and escape sequences for input')                                                 
         parser.add_argument('-E', '--emulated-evdev',  action='store_true', help='Use PTY emulation and evdev for input (single instance)')                                                         
         try:
             args = parser.parse_args()
