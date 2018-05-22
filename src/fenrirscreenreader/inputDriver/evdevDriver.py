4dd3627e9b8f7ba2949ef0612efe17259115e0f7
chrys <chrys87@web.de>
2018-05-22 20:55:33 +0200

    add debug

--- src/fenrirscreenreader/inputDriver/evdevDriver.py
+++ src/fenrirscreenreader/inputDriver/evdevDriver.py
@@ -327,6 +327,7 @@ class driver(inputDriver):
             return
         try:
             self.iDevices[fd].grab()
+            self.env['runtime']['debug'].writeDebugOut("Device grabbed: " + self.iDevices[fd].name, debug.debugLevel.INFO)
             self.gDevices[fd] = True                        
         except IOError:            
             self.gDevices[fd] = True            
@@ -337,7 +338,8 @@ class driver(inputDriver):
             return      
         try:
             self.gDevices[fd] = False                    
-            self.iDevices[fd].ungrab()            
+            self.iDevices[fd].ungrab()   
+            self.env['runtime']['debug'].writeDebugOut("Device ungrabbed: " + self.iDevices[fd].name, debug.debugLevel.INFO)                     
         except:
             pass    
     def removeDevice(self,fd):
