8538dc52a5b4dc280cf770e03177e3ab07aaa25a
chrys87 <chrys87@web.de>
2018-05-23 14:30:39 +0200

    Update evdevDriver.py

--- src/fenrirscreenreader/inputDriver/evdevDriver.py
+++ src/fenrirscreenreader/inputDriver/evdevDriver.py
@@ -66,14 +66,15 @@ class driver(inputDriver):
         monitor.start()
         while active.value:
             validDevices = []
-            device = monitor.poll(1)
+             action, device = monitor.poll(1)
             while device:
                 try:
                     if not '/sys/devices/virtual/input/' in device.sys_path:
                         if device.device_node:
                             if device.sys_name.startswith('event'):                            
-                                validDevices.append(str(device.device_node))
-                    device = monitor.poll(0.1)
+                                if action == 'add':
+                                    validDevices.append(str(device.device_node))
+                     action, device = monitor.poll(0.1)
                 except:                    
                     pass
             if validDevices:
