43ddfb27873b82e2936f9ac6e3ca4318d91a31cc
chrys <chrys87@web.de>
2018-06-11 19:53:10 +0200

    make barrir mode toggle

---
+++ src/fenrirscreenreader/commands/commands/toggle_barrier.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from fenrirscreenreader.core import debug
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass 
+    def getDescription(self):
+        return _('enables or disables the barrier mode')        
+    
+    def run(self):
+        self.env['runtime']['settingsManager'].setSetting('focus', 'barrier', str(not self.env['runtime']['settingsManager'].getSettingAsBool('focus', 'barrier')))
+        if self.env['runtime']['settingsManager'].getSettingAsBool('focus', 'barrier'): 
+            self.env['runtime']['outputManager'].presentText(_("barrier mode enabled"), soundIcon='', interrupt=True)
+        else:
+            self.env['runtime']['outputManager'].presentText(_("barrier mode disabled"), soundIcon='', interrupt=True)
+    
+    def setCallback(self, callback):
+        pass
