45971a8acc90396764755b52e16bb6304f41967f
chrys <chrys87@web.de>
2018-07-07 16:46:48 +0200

    sync

---
+++ src/fenrirscreenreader/commands/onScreenChanged/70000-barrier_detect.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from fenrirscreenreader.core import debug
+
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def getDescription(self):
+        return 'No Description found'      
+
+    def run(self):
+        if not self.env['runtime']['settingsManager'].getSettingAsBool('barrier','enabled'):
+            return
+
+        self.env['runtime']['barrierManager'].handleLineBarrier(self.env['screen']['newContentText'].split('\n'), self.env['screen']['newCursor']['x'],self.env['screen']['newCursor']['y'])
+
+    def setCallback(self, callback):
+        pass
+
