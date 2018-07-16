46b81b5587da65619d775ba06a103313b72922f1
chrys <chrys87@web.de>
2018-07-16 23:23:29 +0200

    branch release 1.9.2

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from shutil import copyfile
 from setuptools import find_packages
 from setuptools import setup
 
-fenrirVersion = '1.9'
-packageVersion = 'post2'
+fenrirVersion = '1.9.2'
+packageVersion = 'post1'
 
 # handle flags for package manager like yaourt and pacaur.
 forceSettings = False
