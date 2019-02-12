81a20e2b954bf8414208a7488e7786284ea492b6
chrys <chrys@linux-a11y.org>
2019-02-13 00:46:46 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 fenrirVersion = '1.9.3'
-packageVersion = 'post2'
+packageVersion = 'post4'
 
 # handle flags for package manager like yaourt and pacaur.
 forceSettings = False
