b23ef94bec124041bfef694ac207e95cc0de8e7d
Storm Dragon <stormdragon2976@gmail.com>
2019-04-27 11:06:46 -0400

    First pass at saving settings.

---
+++ src/fenrirscreenreader/commands/commands/save_settings.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+
+from fenrirscreenreader.core import debug
+from fenrirscreenreader.core import settingsManager
+
+class command():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def getDescription(self):
+        return 'Saves your current Fenrir settings so they are the default.'
+    def run(self):
+    def saveSettings(self):
+        self.env['runtime']['settingsManager'].saveSettings(settingConfigPath)
+    def setCallback(self, callback):
+        pass
