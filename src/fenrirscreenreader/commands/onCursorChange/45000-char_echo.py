975efcace06b15e5cef5a7f6e54fcdf65af8078c
Chrys <chrys@linux-a11y.org>
2019-08-22 00:01:05 +0200

    Revert "resort word echo"
    This reverts commit 3bd8e5e561b93ce22985a46eec869aad1b8745b7.

--- src/fenrirscreenreader/commands/onCursorChange/45000-char_echo.py
+++ src/fenrirscreenreader/commands/onCursorChange/45000-char_echo.py
@@ -20,7 +20,7 @@ class command():
         # big changes are no char (but the value is bigger than one maybe the differ needs longer than you can type, so a little strange random buffer for now)
         xMove = abs(self.env['screen']['newCursor']['x'] - self.env['screen']['oldCursor']['x'])
         if xMove > 1:
-            return
+            return       
         if not self.env['runtime']['settingsManager'].getSettingAsBool('keyboard', 'charEcho'):
             return
         # detect deletion or chilling 
