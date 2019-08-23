c7f62cd1ffa497161966fb5115e2f8949a1ca8d9
chrys <chrys@linux-a11y.org>
2019-08-23 10:06:29 +0200

    Update 15000-char_echo.py

--- src/fenrirscreenreader/commands/onCursorChange/15000-char_echo.py
+++ src/fenrirscreenreader/commands/onCursorChange/15000-char_echo.py
@@ -17,11 +17,12 @@ class command():
         return 'No Description found'
 
     def run(self):
+        # enabled?
+        if not self.env['runtime']['settingsManager'].getSettingAsBool('keyboard', 'charEcho'):
+            return
         # big changes are no char (but the value is bigger than one maybe the differ needs longer than you can type, so a little strange random buffer for now)
         xMove = abs(self.env['screen']['newCursor']['x'] - self.env['screen']['oldCursor']['x'])
-        if xMove > 1:
-            return
-        if not self.env['runtime']['settingsManager'].getSettingAsBool('keyboard', 'charEcho'):
+        if xMove > 2:
             return
         # detect deletion or chilling 
         if self.env['screen']['newCursor']['x'] <= self.env['screen']['oldCursor']['x']:
