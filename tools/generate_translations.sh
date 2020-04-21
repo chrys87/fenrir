26284cc480cfc4e4957dd1891ef56afba2ff3543
Рыбин Денис <mejdugorka@yandex.ru>
2020-04-21 09:41:43 +0700

    Update generate_translations.sh
     user root for raspberry

--- tools/generate_translations.sh
+++ tools/generate_translations.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-python C:\python37\Tools\i18n\pygettext.py -d fenrir ../src/fenrirscreenreader/*.py ../src/fenrirscreenreader/*/*.py ../src/fenrirscreenreader/*/*/*.py
+sudo pygettext3 -d fenrir ../src/fenrirscreenreader/*.py ../src/fenrirscreenreader/*/*.py ../src/fenrirscreenreader/*/*/*.py
