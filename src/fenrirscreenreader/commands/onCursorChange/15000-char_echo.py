05e8fd6791c830e41a7d6bea4c64d9f034ad4079
Chrys <chrys@linux-a11y.org>
2022-02-22 20:25:39 +0100

    add some other debugging

--- src/fenrirscreenreader/commands/onCursorChange/15000-char_echo.py
+++ src/fenrirscreenreader/commands/onCursorChange/15000-char_echo.py
@@ -19,7 +19,7 @@ class command():
     def run(self):
         # enabled? 
         active = self.env['runtime']['settingsManager'].getSettingAsInt('keyboard', 'charEchoMode')
-        self.env['runtime']['debug'].writeDebugOut('charEchoMode: ' + str(active) + ' CAPS:' + str(self.env['input']['newCapsLock'] == True),debug.debugLevel.INFO)
+        self.env['runtime']['debug'].writeDebugOut('charEchoMode: ' + str(active) + ' CAPS:' + str(self.env['input']['newCapsLock'] == True) + str(type(self.env['input']['newCapsLock'])) ,debug.debugLevel.INFO)
 
         # 0 = off
         if active == 0:
