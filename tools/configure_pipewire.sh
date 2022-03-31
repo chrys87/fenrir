e24ad9ec1106057c3ec611391ec957170b976d5b
Storm Dragon <stormdragon2976@gmail.com>
2022-03-31 02:51:04 -0400

    Pipewire script updated.

--- tools/configure_pipewire.sh
+++ tools/configure_pipewire.sh
@@ -5,27 +5,162 @@
 if [[ $(whoami) != "root" ]]; then
 # Get the current user's XDG_HOME
 xdgPath="${XDG_CONFIG_HOME:-$HOME/.config}"
-mkdir -p "$xdgPath/pipewire/pipewire-pulse.conf.d"
+mkdir -p "$xdgPath/pipewire"
 mkdir -p "$xdgPath/wireplumber/main.lua.d"
 mkdir -p "$xdgPath/wireplumber/bluetooth.lua.d"
 
 #create the file that tells the pipewire-pulse server to use a second socket located at /tmp/pulse.sock
-# Warn user if we are going to overwrite an existing 10-console_audio.conf
-if [ -f "$xdgPath/pipewire/pipewire-pulse.conf.d/10-console_audio.conf" ]; then
-    read -p "This will replace the current file located at $xdgPath/pipewire/pipewire-pulse.conf.d/10-console_audio.conf, press enter to continue or control+c to abort. " continue
+# Warn user if we are going to overwrite an existing pipewire-pulse.conf
+if [ -f "$xdgPath/pipewire/pipewire-pulse.conf" ]; then
+    read -p "This will replace the current file located at $xdgPath/pipewire/pipewire-pulse.conf, press enter to continue or control+c to abort. " continue
 fi
-echo 'context.modules = [
-{ name = libpipewire-module-protocol-pulse
-        args = {
-server.address = [
-                    "unix:/tmp/pulse.sock"
-]
-        }
+cat << "EOF" > "$xdgPath/pipewire/pipewire-pulse.conf"
+# PulseAudio config file for PipeWire version "0.3.49" #
+#
+# Copy and edit this file in /etc/pipewire for system-wide changes
+# or in ~/.config/pipewire for local changes.
+#
+# It is also possible to place a file with an updated section in
+# /etc/pipewire/pipewire-pulse.conf.d/ for system-wide changes or in
+# ~/.config/pipewire/pipewire-pulse.conf.d/ for local changes.
+#
+
+context.properties = {
+    ## Configure properties in the system.
+    #mem.warn-mlock  = false
+    #mem.allow-mlock = true
+    #mem.mlock-all   = false
+    #log.level       = 2
+
+    #default.clock.quantum-limit = 8192
 }
+
+context.spa-libs = {
+    audio.convert.* = audioconvert/libspa-audioconvert
+    support.*       = support/libspa-support
+}
+
+context.modules = [
+    { name = libpipewire-module-rt
+        args = {
+            nice.level   = -11
+            #rt.prio      = 88
+            #rt.time.soft = -1
+            #rt.time.hard = -1
+        }
+        flags = [ ifexists nofail ]
+    }
+    { name = libpipewire-module-protocol-native }
+    { name = libpipewire-module-client-node }
+    { name = libpipewire-module-adapter }
+    { name = libpipewire-module-metadata }
+
+    { name = libpipewire-module-protocol-pulse
+        args = {
+	    # contents of pulse.properties can also be placed here
+	    # to have config per server.
+        }
+    }
 ]
+
+# Extra modules can be loaded here. Setup in default.pa can be moved here
 context.exec = [
+    { path = "pactl"        args = "load-module module-always-sink" }
     { path = "pactl"        args = "load-module module-switch-on-connect" }
-]' > $xdgPath/pipewire/pipewire-pulse.conf.d/10-console_audio.conf
+    #{ path = "/usr/bin/sh"  args = "~/.config/pipewire/default.pw" }
+]
+
+stream.properties = {
+    #node.latency          = 1024/48000
+    #node.autoconnect      = true
+    #resample.quality      = 4
+    #channelmix.normalize  = false
+    #channelmix.mix-lfe    = false
+    #channelmix.upmix      = true
+    #channelmix.upmix-method = simple  # none, psd
+    #channelmix.lfe-cutoff = 120
+    #channelmix.fc-cutoff  = 6000
+    #channelmix.rear-delay = 12.0
+    #channelmix.stereo-widen = 0.1
+    #channelmix.hilbert-taps = 0
+}
+
+pulse.properties = {
+    # the addresses this server listens on
+    server.address = [
+        "unix:native"
+        "unix:/tmp/pulse.sock"              # absolute paths may be used
+        #"tcp:4713"                         # IPv4 and IPv6 on all addresses
+        #"tcp:[::]:9999"                    # IPv6 on all addresses
+        #"tcp:127.0.0.1:8888"               # IPv4 on a single address
+        #
+        #{ address = "tcp:4713"             # address
+        #  max-clients = 64                 # maximum number of clients
+        #  listen-backlog = 32              # backlog in the server listen queue
+        #  client.access = "restricted"     # permissions for clients
+        #}
+    ]
+    #pulse.min.req          = 256/48000     # 5ms
+    #pulse.default.req      = 960/48000     # 20 milliseconds
+    #pulse.min.frag         = 256/48000     # 5ms
+    #pulse.default.frag     = 96000/48000   # 2 seconds
+    #pulse.default.tlength  = 96000/48000   # 2 seconds
+    #pulse.min.quantum      = 256/48000     # 5ms
+    #pulse.default.format   = F32
+    #pulse.default.position = [ FL FR ]
+    # These overrides are only applied when running in a vm.
+    vm.overrides = {
+        pulse.min.quantum = 1024/48000      # 22ms
+    }
+}
+
+# client/stream specific properties
+pulse.rules = [
+    {
+        matches = [
+            {
+                # all keys must match the value. ~ starts regex.
+                #client.name                = "Firefox"
+                #application.process.binary = "teams"
+                #application.name           = "~speech-dispatcher.*"
+            }
+        ]
+        actions = {
+            update-props = {
+                #node.latency = 512/48000
+            }
+            # Possible quirks:"
+            #    force-s16-info                 forces sink and source info as S16 format
+            #    remove-capture-dont-move       removes the capture DONT_MOVE flag
+            #quirks = [ ]
+        }
+    }
+    {
+        # skype does not want to use devices that don't have an S16 sample format.
+        matches = [
+             { application.process.binary = "teams" }
+             { application.process.binary = "skypeforlinux" }
+        ]
+        actions = { quirks = [ force-s16-info ] }
+    }
+    {
+        # firefox marks the capture streams as don't move and then they
+        # can't be moved with pavucontrol or other tools.
+        matches = [ { application.process.binary = "firefox" } ]
+        actions = { quirks = [ remove-capture-dont-move ] }
+    }
+    {
+        # speech dispatcher asks for too small latency and then underruns.
+        matches = [ { application.name = "~speech-dispatcher*" } ]
+        actions = {
+            update-props = {
+                pulse.min.req          = 1024/48000     # 21ms
+                pulse.min.quantum      = 1024/48000     # 21ms
+            }
+        }
+    }
+]
+EOF
 
 #Creates the file that tells pipewire not to suspend any sinks for all devices. This makes sure audio doesn't die after switching to the console.
 # Warn user if we are going to overwrite an existing 50-do-not-suspend.lua
