56f403b0b94e9940d09abb3e499f51f79daabf77
Storm Dragon <stormdragon2976@gmail.com>
2024-11-20 03:54:14 -0500

    Attempt to fix the weird bug where marking any but the first column of the line, then marking on another line causes nothing to be copied.

--- src/fenrirscreenreader/commands/commands/copy_marked_to_clipboard.py
+++ src/fenrirscreenreader/commands/commands/copy_marked_to_clipboard.py
@@ -10,12 +10,49 @@ from fenrirscreenreader.utils import mark_utils
 class command():
     def __init__(self):
         pass
+        
     def initialize(self, environment):
         self.env = environment
+        
     def shutdown(self):
         pass 
+        
     def getDescription(self):
         return _('copies marked text to the currently selected clipboard')    
+    
+    def getTextFromScreen(self, startMark, endMark):
+        screenContent = self.env['screen']['newContentText']
+        screenLines = screenContent.split('\n')
+        
+        startY = min(startMark['y'], len(screenLines) - 1)
+        endY = min(endMark['y'], len(screenLines) - 1)
+        
+        # If marks are on the same line
+        if startY == endY:
+            line = screenLines[startY]
+            startX = min(startMark['x'], len(line))
+            endX = min(endMark['x'], len(line))
+            return line[startX:endX]
+            
+        # Handle multi-line selection
+        result = []
+        
+        # First line (from start mark to end of line)
+        firstLine = screenLines[startY]
+        startX = min(startMark['x'], len(firstLine))
+        result.append(firstLine[startX:])
+        
+        # Middle lines (complete lines)
+        for lineNum in range(startY + 1, endY):
+            result.append(screenLines[lineNum])
+            
+        # Last line (from start to end mark)
+        if endY > startY:
+            lastLine = screenLines[endY]
+            endX = min(endMark['x'], len(lastLine))
+            result.append(lastLine[:endX])
+            
+        return '\n'.join(result)
     
     def run(self):
         if not self.env['commandBuffer']['Marks']['1']:
@@ -23,17 +60,18 @@ class command():
             return
         if not self.env['commandBuffer']['Marks']['2']:
             self.env['runtime']['cursorManager'].setMark()
-
+            
         # use the last first and the last setted mark as range
         startMark = self.env['commandBuffer']['Marks']['1'].copy()
         endMark = self.env['commandBuffer']['Marks']['2'].copy()         
         
-        marked = mark_utils.getTextBetweenMarks(startMark, endMark, self.env['screen']['newContentText'])
+        # Replace mark_utils.getTextBetweenMarks with our new method
+        marked = self.getTextFromScreen(startMark, endMark)
+        
         self.env['runtime']['memoryManager'].addValueToFirstIndex('clipboardHistory', marked)
         # reset marks
         self.env['runtime']['cursorManager'].clearMarks()      
-
         self.env['runtime']['outputManager'].presentText(marked, soundIcon='CopyToClipboard', interrupt=True)
-
+        
     def setCallback(self, callback):
         pass
